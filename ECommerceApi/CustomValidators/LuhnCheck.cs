5452a4f53b456fe37679aafb36a8fae4da1d293e
Jeffry Gonzalez <jeff@hypertheory.com>
2021-10-26 16:31:53 -0400

    before practice

---
+++ ECommerceApi/CustomValidators/LuhnCheck.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.CustomValidators
+{
+    public class LuhnCheck
+    {
+        public bool PassesLuhnCheck(string creditCardNumber)
+        {
+            return false;
+        }
+    }
+}
