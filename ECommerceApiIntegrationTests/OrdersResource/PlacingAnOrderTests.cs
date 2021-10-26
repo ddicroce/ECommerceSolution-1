5144b30246698983adf598e61aed9a963d0a1002
Jeffry Gonzalez <jeff@hypertheory.com>
2021-10-26 14:37:55 -0400

    first test written

---
+++ ECommerceApiIntegrationTests/OrdersResource/PlacingAnOrderTests.cs
@@ -0,0 +1,33 @@
+﻿using ECommerceApi;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ECommerceApiIntegrationTests.OrdersResource
+{
+    public class PlacingAnOrderTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+
+        private readonly WebApplicationFactory<Startup> _factory;
+        private readonly HttpClient _client;
+        public PlacingAnOrderTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+            _client = _factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Gets201StatusCode()
+        {
+            var response = await _client.PostAsync("/orders", null);
+
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        }
+    }
+}
