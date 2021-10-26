efd3205cbb6278d10689ca928006694c04afe76d
Jeffry Gonzalez <jeff@hypertheory.com>
2021-10-26 14:54:37 -0400

    another test

---
+++ ECommerceApi/Controllers/OrdersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Controllers
+{
+    public class OrdersController : ControllerBase
+    {
+        [HttpPost("/orders")]
+        public async Task<ActionResult> PlaceOrder()
+        {
+            return StatusCode(201);
+        }
+    }
+}
--- ECommerceApiIntegrationTests/OrdersResource/PlacingAnOrderTests.cs
+++ ECommerceApiIntegrationTests/OrdersResource/PlacingAnOrderTests.cs
@@ -29,5 +29,62 @@ namespace ECommerceApiIntegrationTests.OrdersResource
 
             Assert.Equal(HttpStatusCode.Created, response.StatusCode);
         }
+        [Fact]
+        public async Task HasCorrectContentTypeOnResponse()
+        {
+            var sampleData = new OrderRequest
+            {
+                name = "Bob Smith",
+                address = "1212 Orange St",
+                city = "Akron",
+                state = "OH",
+                zip = "44319",
+                creditCardInfo = new Creditcardinfo
+                {
+                    number = "555-55-5555",
+                    expiration = "06/22",
+                    cvv2 = "973"
+                },
+                items = new List<Item>
+                {
+                    new Item { id="1", name="Beer", price=6.99M, qty=1}
+                }
+
+
+            };
+
+            var response = await _client.PostAsJsonAsync("/orders", sampleData);
+
+            Assert.Equal("application/json", response.Content.Headers.ContentType.MediaType);
+        }
     }
+
+
+    public class OrderRequest
+    {
+        public string name { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+        public Creditcardinfo creditCardInfo { get; set; }
+        public List<Item> items { get; set; }
+    }
+
+    public class Creditcardinfo
+    {
+        public string number { get; set; }
+        public string expiration { get; set; }
+        public string cvv2 { get; set; }
+    }
+
+    public class Item
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public int qty { get; set; }
+        public decimal price { get; set; }
+    }
+
+
 }
